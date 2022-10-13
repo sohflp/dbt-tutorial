0684a8259c0ada7b3f558dbc3f507612bbfb5431
Felipe Rodrigues <sohflp@gmail.com>
2022-10-13 13:48:03 +1100

    Add metrics based on payment methods

--- demo/models/sales/marts/mart_orders.sql
+++ demo/models/sales/marts/mart_orders.sql
@@ -3,9 +3,14 @@
 
 {{ config(materialized='table') }}
 
+{% set payment_methods = ["bank_transfer", "credit_card", "coupon", "gift_card"] %}
+
 select customer_id,
        full_name,
        count(*) as total_orders,
+       {% for payment_method in payment_methods %}
+       sum(case when payment_method = '{{payment_method}}' then amount end) as {{payment_method}}_amount,
+       {% endfor %}
        sum(amount) as total_amount
 from {{ ref('stg_customers') }}
 left join {{ ref('stg_orders') }}
